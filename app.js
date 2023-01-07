67b422e7f15853e89b2f26bdb82209e74b6a3fd2
FightlolYes <yashpatil.uwu@gmail.com>
2023-01-07 13:28:50 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+quote = ""
+
+function fetchQuote () {
+        fetch("https://api.kanye.rest/")
+        .then(response => response.json())
+        .then((data) => {
+            quote = data.quote
+
+            $(".quote").text(quote)
+            console.log(quote)
+        })
+}
+
+fetchQuote()
+
+$(document).ready(fetchQuote);
